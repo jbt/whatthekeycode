19a90ad7d36fff92f94d3d84f60cbefc28fc59dc
JT <jt@gosquared.com>
2012-09-05 09:51:43 +0100

    This.

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>What Key Code?</title>
+  <title>What The Key Code?</title>
   <style type="text/css">
-    #kc {
+    body {
+      font-weight: 100;
+      font-family: "DINPro", "Museo Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+      background: #f7f5ef
+    }
+    #kc, #m {
       position: fixed;
       font-size: 200px;
       text-align: center;
       width: 100%;
       left: 0;
       height: 200px;
+      line-height: 200px;
       top: 50%;
       margin-top: -100px;
+      color: #a89c8d;
     }
+    #m { font-size: 80px; }
   </style>
 </head>
 <body>
+  <div id="m">Press a key, any key.</div>
   <div id="kc"></div>
   <script>
     document.onkeydown = function(e){
+      document.getElementById('m').innerHTML = '';
       document.getElementById('kc').innerHTML = e.keyCode || e.which;
     };
   </script>
